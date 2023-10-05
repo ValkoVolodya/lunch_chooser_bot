2c5aec674a3f2ae0b91babde4f675d6d7dcc8010
v.valko <v.valko@smartweb.com.ua>
2023-10-05 22:15:01 +0300

    [add] basic functionality

--- lunch_chooser_bot.py
+++ lunch_chooser_bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 
 from telegram import ForceReply, Update
 from telegram.ext import (
@@ -19,6 +20,8 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+PLACES = []
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
@@ -32,6 +35,20 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text("Help!")
 
 
+async def add_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    place_name = update.message.text.removeprefix("/add ")
+    PLACES.append({"name": place_name})
+    await update.message.reply_text(
+        f'Заклад {place_name} додано до сьогоднішнього списку варіантів'
+    )
+
+
+async def choose_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if len(PLACES) < 1:
+        await update.message.reply_text("Не можу вибрати заклад, бо немає варіантів(")
+    await update.message.reply_text(f'Сьогодні вибрано {random.choice(PLACES)["name"]}')
+
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
@@ -41,6 +58,8 @@ def main() -> None:
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("add", add_command))
+    application.add_handler(CommandHandler("choose", choose_command))
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
